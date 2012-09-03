ee6754e5885f34ed758aeb002d37dbb102e518b6
Robert Warner <radicalphoenix@gmail.com>
2012-09-03 03:41:50 -0700

    fixed manager registers

--- hipflask.py
+++ hipflask.py
@@ -144,37 +144,38 @@ class HipFlask(object):
                 raise wtf.ValidationError('Duplicate username')
 
     def register_manager_commands(self, manager):
-        manager.command(self.makedb)
-        manager.command(self.make_migration)
-        manager.command(self.test_migration)
-        manager.command(self.migrate)
-        manager.command(self.version)
 
-    def makedb(self, force=False):
-        '''Create a new database from scratch, put it under version control, and run all migrations'''
-        if os.path.exists(self.config.DB_FILE):
-            if force:
-                os.remove(self.config.DB_FILE)
-            else:
-                raise Exception('Database already exists! Use -f to force file deletion')
+        @manager.command
+        def makedb(force=False):
+            '''Create a new database from scratch, put it under version control, and run all migrations'''
+            if os.path.exists(self.config.DB_FILE):
+                if force:
+                    os.remove(self.config.DB_FILE)
+                else:
+                    raise Exception('Database already exists! Use -f to force file deletion')
 
-        sqlite3.connect(self.config.DB_FILE)
-        migrate_api.version_control(self.config.DB_URL, self.config.REPOSITORY)
-        self.migrate_up()
+            sqlite3.connect(self.config.DB_FILE)
+            migrate_api.version_control(self.config.DB_URL, self.config.REPOSITORY)
+            self.migrate_up()
 
-    def make_migration(self, description):
-        migrate_api.script(description, self.config.REPOSITORY)
+        @manager.command
+        def make_migration(description):
+            migrate_api.script(description, self.config.REPOSITORY)
 
-    def test_migration(self):
-        shutil.copy2(self.config.DB_FILE, self.config.TEST_DB_FILE)
-        migrate_api.test(self.config.TEST_DB_URL, self.config.REPOSITORY)
-        os.remove(self.config.TEST_DB_FILE)
+        @manager.command
+        def test_migration():
+            shutil.copy2(self.config.DB_FILE, self.config.TEST_DB_FILE)
+            migrate_api.test(self.config.TEST_DB_URL, self.config.REPOSITORY)
+            os.remove(self.config.TEST_DB_FILE)
 
-    def version(self):
-        migrate_api.version(self.config.REPOSITORY)
+        @manager.command
+        def version():
+            print migrate_api.version(self.config.REPOSITORY)
 
-    def migrate(self):
-        self.migrate_up()
+        @manager.command
+        def migrate():
+            self.migrate_up()
 
-    def migrate_up(self):
-        migrate_api.upgrade(self.config.DB_URL, self.config.REPOSITORY)
+        @manager.command
+        def migrate_up():
+            migrate_api.upgrade(self.config.DB_URL, self.config.REPOSITORY)
