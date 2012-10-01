56def833500846bf355f1f4440123f4db7827333
Robert Warner <radicalphoenix@gmail.com>
2012-09-30 22:20:26 -0700

    added model definitions for graph representation experiment

--- hipflask/models.py
+++ hipflask/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Boolean, Integer, String, Sequence
+from sqlalchemy import Column, Boolean, Integer, String, Sequence, relationship, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+import sys
 
 Base = declarative_base()
 
@@ -29,3 +30,47 @@ class User(Base):
 
     def get_id(self):
         return self.id
+
+
+class NodeType(Base):
+    __tablename__ = 'nodetypes'
+
+    id = Column(Integer, Sequence('nodetype_id_seq'), primary_key=True)
+    name = Column(String(255), unique=True)
+    nodes = relationship("Node", backref="type")
+
+    @property
+    def clazz(self):
+        return getattr(sys.modules[__name__], self.name)
+
+
+class Node(Base):
+    __tablename__ = 'nodes'
+
+    id = Column(Integer, Sequence('node_id_seq'), primary_key=True)
+    nodetype_id = Column(Integer, ForeignKey('nodetypes.id'))
+    object_id = Column(Integer)
+
+    incoming_edges = relationship("Edge", backref="to_nodes", primaryjoin="Node.id == Edge.to_id")
+    outgoing_edges = relationship("Edge", backref="from_nodes", primaryjoin="Node.id == Edge.from_id")
+
+    @property
+    def object(self):
+        return self.db.session.query(self.type.clazz).get(self.object_id)
+
+
+class Edge(Base):
+    __tablename__ = 'edges'
+
+    id = Column(Integer, Sequence('edge_id_seq'), primary_key=True)
+    from_id = Column(Integer, ForeignKey('nodes.id'))
+    to_id = Column(Integer, ForeignKey('nodes.id'))
+    edgetype_id = Column(Integer, ForeignKey('edgetypes.id'))
+
+
+class EdgeType(Base):
+    __tablename__ = 'edgetypes'
+
+    id = Column(Integer, Sequence('edgetype_id_seq'), primary_key=True)
+    name = Column(String(255), unique=True)
+    edges = relationship("Edge", backref="type")
